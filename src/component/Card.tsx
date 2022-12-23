2499d7114729825f1cdc4b7d9209de012b399005
Anton <98453976+anton12042001@users.noreply.github.com>
2022-12-23 13:52:56 +0700

    studying typescript

---
+++ src/component/Card.tsx
@@ -0,0 +1,32 @@
+import React, {FC, PropsWithChildren} from 'react';
+
+export enum CardVariant {
+    outlined = "outlined",
+    primary = "primary",
+}
+
+
+interface CardProps{
+    width?:string
+    height?:string
+    variant: CardVariant
+
+}
+
+const Card: FC<PropsWithChildren<CardProps>> =
+    ({
+         width
+         ,height
+         ,children
+         ,variant
+    }) => {
+    return (
+        <div style={{width,height, border: variant === CardVariant.outlined ? '1px solid gray' : "none",
+            background:variant === CardVariant.primary ? "lightgray" : " "
+        }} >
+            {children}
+        </div>
+    );
+};
+
+export default Card;
